b3496a3fa238417a2625f7bd4c578f69b6adae40
Junior7 <giftmomanyi97@gmail.com>
2025-08-03 08:23:57 +0300

    Sermon:Added Sabbaths Sermon

--- src/components/home/MediaSection.tsx
+++ src/components/home/MediaSection.tsx
@@ -28,9 +28,9 @@ const MediaSection = () => {
 
   // These would be replaced with actual video IDs from the church's YouTube channel
   const sermonVideos = [
+    { id: "x-ot9xGKkZ0?si=1GAeoe3aMwNoqmDK", title: "Lost But Not Forgotten" }, 
     { id: "zHmiP1nN3TM?si=EU2u3rEeAwFRY0dc", title: "Rock Solid With God" }, 
     { id: "u_103Y7ZuHM?si=DLQjoK-G89AftwsQ", title: "The Secret of Giving and Living" }, 
-    { id: "nsAcW67cu2Y?si=ZZOc2u-fc-DQc4-A&amp;start=12571", title: "Ransomed & Glorified" }, 
   ];
   
   const musicVideos = [
