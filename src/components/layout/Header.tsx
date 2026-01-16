eaecb543e21e4b72028e873dbddf57455c82b249
Junior7 <giftmomanyi97@gmail.com>
2026-01-16 10:44:18 +0300

    chore(menu):removed portal menu from display for sceurity

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -50,7 +50,6 @@ const Header = () => {
         { name: 'Membership Transfer', path: '/membership-transfer' },
         { name: 'Benevolence', path: '/benevolence' },
         { name: 'Library', path: '/library' },
-        { name: 'Portal', path: '/admin/login' },
       ]
     },
     { name: 'Contact', path: '/contact' },
